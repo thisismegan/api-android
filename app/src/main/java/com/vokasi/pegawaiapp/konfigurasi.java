071070cf7df48b7232d5e9f764a374b92296759b
Megan <thisismegantoro@gmail.com>
2020-10-14 23:24:32 +0700

    api-android

---
+++ app/src/main/java/com/vokasi/pegawaiapp/konfigurasi.java
@@ -0,0 +1,13 @@
+package com.vokasi.pegawaiapp;
+
+public class konfigurasi {
+    public static String BASE_URL="192.168.43.6/pegawai";
+    public static final String URL_ADD=BASE_URL+"/tambahpgw.php";
+    public static final String URL_GET_ALL=BASE_URL+"/tampilsemuapgw.php";
+
+    //json tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID="id";
+    public static final String TAG_NAME="name";
+
+}
